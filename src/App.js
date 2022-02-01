a7a0cf9fc26885f2f4e32df255642146867e1ac9
Mariam <mariammdanielyan@gmail.com>
2022-02-02 00:16:55 +0400

    Making Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import Header from "./components/Header";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Routes,
+    Link
+} from "react-router-dom";
+import Home from "./components/Home";
+import Edit from "./components/Edit";
+import Search from "./components/Search";
+import Login from "./components/Login";
 
 
 const App =()=> {
   return (
     <>
+      <Header />
+        <Routes>
+            <Route path='home' element={<Home />} />
+            <Route path='edit' element={<Edit/>} />
+            <Route path='search' element={<Search />} />
+            <Route path='login' element={<Login />} />
+        </Routes>
     </>
   );
 }
