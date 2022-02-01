30f1f44c1d13f3fbd57f73d54237c5777ff8e51b
Mariam <mariammdanielyan@gmail.com>
2022-02-02 01:16:54 +0400

    Making Home Component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,31 @@
+import {useEffect, useState} from "react";
+
 const Home = () =>{
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        fetch('/db.json')
+            .then(res => res.json())
+            .then(res => setMessages(res.messages))
+            .catch(e => console.log('error from messagesContext', e))
+    },[])
+
     return (
         <>
-            <div>
-                Home
+            <div className={'home-container'}>
+                {
+                    messages.map(item =>(
+                        <div
+                            key={item.id}
+                            className={'carts'}
+                        >
+                            <p>Name: {item.name}</p>
+                            <p>Date: {item.date}</p>
+                            <p>Text: {item.text}</p>
+
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
