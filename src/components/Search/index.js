6045c325a42c9960c0d1f34401df38cc66a1fec3
Mariam <mariammdanielyan@gmail.com>
2022-02-02 22:29:41 +0400

    commit

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,8 +1,21 @@
+import {TYPE} from "../../helpers/constants";
+
 const Search = () =>{
     return (
         <>
-            <div>
-                Search
+            <div className={'search-container'}>
+
+                   <select className={'search-select'}>
+                       <option value={"Choose"}>Choose</option>
+                       {
+                           TYPE.map((item)=>(
+                                <option value={item}>{item}</option>
+                           ))
+                       }
+                   </select>
+
+                    <input type={'text'} placeholder={'Write key for search!'}/>
+
             </div>
         </>
     )
